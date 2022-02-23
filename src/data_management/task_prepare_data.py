bc9f296f86249fd671ae3d250fe20d40852baa10
maxxxbb <82452765+maxxxbb@users.noreply.github.com>
2022-02-23 13:30:23 +0100

    Added Pytask Dependencies

--- src/data_management/task_prepare_data.py
+++ src/data_management/task_prepare_data.py
@@ -6,16 +6,19 @@ from src.config import BLD
 from src.config import SRC
 
 
-def prepare_data():
+def prepare_data(data, ind):  # at a later point add option for full dataset
     """
+
     Loads dta-file into a Pandas Dataframe and drops individuals for which individual parameters
-    could not be estimated ( total of 28 participants). Called by Pytask
+    could not be estimated ( total of 28 participants).
+    Args:
+        data(Dta-File): Whole Dataset
+        ind(Dta-File): Individuals for which analysis was possible with Stata-Algorithm
 
     Returns:
-        prepared_data (Pandas Dataframe)
+        prepared_data(Pandas Dataframe): Dataset for Analysis
     """
-    data = pd.read_stata(SRC / "original_data" / "decisions_data.dta")
-    ind = pd.read_csv(SRC / "original_data" / "ind_to_keep.csv")
+
     data = data[data.wid.isin(ind.wid_col1)]  # drops all individuals not in 'ind'
     data = data[data.bonusoffered != 1]  # remove observations where a bonus was offered
     data["pb"] = data["workdone1"] / 10
@@ -27,15 +30,15 @@ def prepare_data():
     return data
 
 
-def save_data(data, path):
-    """
-    Serializes Dataframe
-    """
-    data = pd.DataFrame(data)
-    data.to_pickle(path)
-
-
+@pytask.mark.depends_on(
+    [
+        SRC / "Replication_Files" / "original_data" / "decisions_data.dta",
+        SRC / "Replication_Files" / "original_data" / "ind_to_keep.csv",
+    ]
+)
 @pytask.mark.produces(BLD / "data" / "prepared_data.pkl")
-def task_get_prepared_data(produces):
-    prepared_data = prepare_data()
-    save_data(prepared_data, produces)
+def task_get_prepared_data(depends_on, produces):
+    data = pd.read_stata(depends_on[0])
+    ind = pd.read_csv(depends_on[1])
+    prepared_data = prepare_data(data, ind)
+    prepared_data.to_pickle(produces)
