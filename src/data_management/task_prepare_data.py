6e760e6dd388fed35d6d1d16a62deeef40b6d3bb
maxxxbb <82452765+maxxxbb@users.noreply.github.com>
2022-03-08 11:09:28 +0100

    Moved datamanagement into one file

--- src/data_management/task_prepare_data.py
+++ src/data_management/task_prepare_data.py
@@ -1,9 +1,9 @@
+import numpy as np
 import pandas as pd
 import pytask
 
 from src.config import BLD
 from src.config import SRC
-from src.data_management.prepare_data import prepare_data
 
 
 @pytask.mark.depends_on(
@@ -27,3 +27,30 @@ def task_get_prepared_data(depends_on, produces):
 
     prepared_data_full.to_pickle(produces[0])
     prepared_data.to_pickle(produces[1])
+
+
+def prepare_data(data, ind, full_dataset=False):
+    """
+
+    Prepares Data for use in Analysis.
+    removes observatoins where bonus was offerde
+    Args:
+        data(Dta-File): Whole Dataset
+        full_dataset(Boolean): indicates whether individuals should be left
+        out of analysis or not
+    Returns:
+        prepared_data(Pandas Dataframe): Dataset for Analysis
+    """
+    data = data[data.bonusoffered != 1]
+    data["pb"] = data["workdone1"] / 10
+    data["ind_effort10"] = (data["effort"] == 10).astype(int)  # ind_effort10 dummy
+    data["ind_effort110"] = (data["effort"] == 110).astype(int)  # ind_effort110 dummy
+
+    if full_dataset == False:
+        out = data[data.wid.isin(ind.wid_col1)]
+    else:
+        out = data
+
+    out.index = np.arange(len(out.wid))
+
+    return out
